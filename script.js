a835711d39baa3a31acc4cea2df7163ca1c082d9
Matthew Costanzo <Matt@c02lp093fd57.home>
2022-04-22 13:57:55 -0400

    challenge

---
+++ script.js
@@ -0,0 +1,11 @@
+const people = ["George", "Billy", "Bob", "Joe", "Stuart"];
+
+function newPerson(){
+    people.push("Jimmy");
+}
+const numbers = [1, 2, 3, 4, 5];
+numbers.push(6);
+const boolean = [10 > 9, 11 > 2, 15 < 6, 22 > 14, 99 < 23];
+console.log(people);
+console.log(numbers);
+console.log(boolean);
